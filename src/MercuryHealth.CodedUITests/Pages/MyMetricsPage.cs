350d234df379f730a9cb4f8d914601335ea046a0
Abel Wang <xandersparks@hotmail.com>
2016-04-09 12:00:00 -0500

    Initial checkin of source code for Mercury Health demo

---
+++ src/MercuryHealth.CodedUITests/Pages/MyMetricsPage.cs
@@ -0,0 +1,51 @@
+﻿//using Microsoft.VisualStudio.TestTools.UITesting;
+
+//namespace MercuryHEalth.CodedUITests.Pages
+//{
+//    public  class MyMetricsPage
+//    {
+//        private BrowserWindow _browser;
+
+//        public MyMetricsPage(BrowserWindow _browser)
+//        {
+//            this._browser = _browser;
+//        }
+//    }
+//}
+
+
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MercuryHEalth.CodedUITests.Pages
+{
+    public class MyMetricsPage : BasePage
+    {
+        public MyMetricsPage(BrowserWindow browser)
+        {
+            _browser = browser;
+        }
+
+        #region Verifications
+        public MyMetricsPage VerifyMyMetricsPageReached()
+        {
+            try
+            {
+                var createNewLink = new HtmlHyperlink(_browser);
+                createNewLink.SearchProperties.Add(HtmlHyperlink.PropertyNames.InnerText, "CreateNew");
+                createNewLink.SearchProperties.Add(HtmlHyperlink.PropertyNames.AbsolutePath, "/MyMetricsPage/Create");
+                createNewLink.Find();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Could not find create new link and verify my metrics page reached: " + e.Message);
+            }
+            return this;
+
+
+        }
+        #endregion
+    }
+}
---
+++ src/MercuryHealth.UnitTests/HealthCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+using MercuryHealth.Web.Utilities.Fakes;
+using MercuryHealth.Web.Utilities;
+using System.Fakes;
+
+namespace MercuryHealth.UnitTests
+{
+    [TestClass]
+    public class HealthCalculatorTests
+    {
+        [TestMethod]
+        public void GenerateReportNameTest()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimUserRepository.AllInstances.GetUserInt32 = (self, userId) => new User
+                {
+                    Id = userId,
+                    FirstName = "Abel",
+                    LastName = "Wang"
+                };
+
+                ShimDateTime.NowGet = () => new DateTime(2016, 2, 5);
+
+                var calc = new HealthCalculator();
+                var reportName = calc.GenerateReportName(25);
+
+                Assert.AreEqual("2/5/2016 12:00:00 AM: Abel Wang", reportName, "Report name is wrong");
+            }
+        }
+    }
+}
